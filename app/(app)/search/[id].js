7844cab263cb8979c6befe90ef5363ca5eb53bce
Perrier Julien <julien.perrier@etudiant.univ-rennes1.fr>
2024-03-22 23:45:56 +0100

    event search screen

--- app/(app)/search/[id].js
+++ app/(app)/search/[id].js
@@ -1,21 +1,26 @@
 import React, { useEffect, useState } from 'react'
-import { ActivityIndicator, FlatList, Image, TouchableOpacity, View } from 'react-native'
-import { Stack, useRouter, useSearchParams } from 'expo-router'
+import { ActivityIndicator, FlatList, Image, TextInput, TouchableOpacity, View } from 'react-native'
+import { Stack, useGlobalSearchParams, useRouter, useSearchParams } from 'expo-router'
 import { Text, SafeAreaView } from 'react-native'
 
 
 import { ScreenHeaderBtn, NearbyJobCard } from '../../components'
 import { COLORS, icons, SIZES } from '../../constants'
 import styles from '../../styles/search'
+import FilterModal from '../../components/search/FilterModal'
 
-const JobSearch = () => {
-    const params = useSearchParams();
+const EventSearch = () => {
+    const params = useGlobalSearchParams();
     const router = useRouter()
+    const [searchTerm, setSearchTerm] = useState("");
 
     const [searchResult, setSearchResult] = useState([]);
     const [searchLoader, setSearchLoader] = useState(false);
     const [searchError, setSearchError] = useState(null);
     const [page, setPage] = useState(1);
+
+    const [filterModalVisible, setFilterModalVisible] = useState(false);
+
 
     const handleSearch = async () => {
         setSearchLoader(true);
@@ -33,27 +38,78 @@ const JobSearch = () => {
     }
 
     useEffect(() => {
+        console.log("params: ", params);
+        setSearchTerm(params.id)
         handleSearch()
     }, [])
 
+    const handleApplyFilters = (filters) => {
+        // Appliquer les filtres à votre liste de recherche
+        console.log('Filters applied:', filters);
+    };
+
+    const handleResetFilters = () => {
+        // Réinitialiser les filtres
+        console.log('Filters reset');
+    };
+
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-          {/*   <Stack.Screen
-                options={{
-                    headerStyle: { backgroundColor: COLORS.lightWhite },
-                    headerShadowVisible: false,
-                    headerLeft: () => (
-                        <ScreenHeaderBtn
-                            iconUrl={icons.left}
-                            dimension='60%'
-                            handlePress={() => router.back()}
-                        />
-                    ),
-                    headerTitle: "",
+            <View
+                style={{
+                    flex: 1,
+                    padding: SIZES.medium,
                 }}
-            /> */}
+            >
 
-            <FlatList
+                <Stack.Screen
+                    options={{
+                        headerStyle: { backgroundColor: COLORS.lightWhite },
+                        headerShadowVisible: false,
+                        headerLeft: () => (
+                            <ScreenHeaderBtn
+                                iconUrl={icons.left}
+                                dimension='60%'
+                                handlePress={() => router.back()}
+                            />
+                        ),
+                        headerRight: () => (
+                            <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' handlePress={() => setFilterModalVisible(true)} />
+                          ),
+                        headerTitle: "",
+                    }}
+                />
+
+                <FilterModal
+                visible={filterModalVisible}
+                onClose={() => setFilterModalVisible(false)}
+                onApply={handleApplyFilters}
+                onReset={handleResetFilters}
+            />
+
+                <View style={styles.searchContainer}>
+                    <View style={styles.searchWrapper}>
+                        <TextInput
+                            style={styles.searchInput}
+                            value={searchTerm}
+                            onChangeText={(text) => setSearchTerm(text)}
+                            placeholder='Que recherchez vous ?'
+                        />
+                    </View>
+
+                    <TouchableOpacity style={styles.searchBtn} onPress={() => { }}>
+                        <Image
+                            source={icons.search}
+                            resizeMode='contain'
+                            style={styles.searchBtnImage}
+                        />
+                    </TouchableOpacity>
+                </View>
+
+                {/* <Text>Resultats pour : {searchTerm} </Text> */}
+
+
+                {/* <FlatList
                 data={searchResult}
                 renderItem={({ item }) => (
                     <NearbyJobCard
@@ -78,6 +134,7 @@ const JobSearch = () => {
                         </View>
                     </>
                 )}
+
                 ListFooterComponent={() => (
                     <View style={styles.footerContainer}>
                         <TouchableOpacity
@@ -105,9 +162,10 @@ const JobSearch = () => {
                         </TouchableOpacity>
                     </View>
                 )}
-            />
+            /> */}
+            </View>
         </SafeAreaView>
     )
 }
 
-export default JobSearch
+export default EventSearch
--- app/styles/search.js
+++ app/styles/search.js
@@ -20,6 +20,7 @@ const styles = StyleSheet.create({
     loaderContainer: {
         marginTop: SIZES.medium
     },
+
     footerContainer: {
         marginTop: SIZES.small,
         justifyContent: 'center',
@@ -52,7 +53,42 @@ const styles = StyleSheet.create({
         fontFamily: FONT.bold,
         fontSize: SIZES.medium,
         color: COLORS.primary
-    }
+    },
+    searchContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        marginTop: SIZES.large,
+        height: 50,
+      },
+      searchWrapper: {
+        flex: 1,
+        backgroundColor: COLORS.white,
+        marginRight: SIZES.small,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: SIZES.medium,
+        height: "100%",
+      },
+      searchInput: {
+        fontFamily: FONT.regular,
+        width: "100%",
+        height: "100%",
+        paddingHorizontal: SIZES.medium,
+      },
+      searchBtn: {
+        width: 50,
+        height: "100%",
+        backgroundColor: COLORS.tertiary,
+        borderRadius: SIZES.medium,
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      searchBtnImage: {
+        width: "50%",
+        height: "50%",
+        tintColor: COLORS.white,
+      },
 });
 
 export default styles;

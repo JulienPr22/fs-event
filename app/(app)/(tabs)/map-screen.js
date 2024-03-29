8117c00cf1b9a3dcad84f9217acab4b3e98ca6e9
Perrier Julien <julien.perrier@etudiant.univ-rennes1.fr>
2024-03-29 23:36:04 +0100

    update map-screen

--- app/(app)/(tabs)/map-screen.js
+++ app/(app)/(tabs)/map-screen.js
@@ -1,9 +1,11 @@
-import { Text, View, } from 'react-native';
+import { Alert, Text, View, } from 'react-native';
 import { StyleSheet } from 'react-native';
 import { useEffect, useRef, useState } from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 import firestoreService from '../services/fireStoreService';
 import * as Location from 'expo-location';
+import { Tabs, useRouter } from 'expo-router';
+import { SIZES } from '../../constants';
 
 
 const INITIAL_REGION = {
@@ -14,7 +16,8 @@ const INITIAL_REGION = {
 }
 
 function MapScreen() {
-  // const mapRef = useRef < MapView();
+  const router = useRouter();
+  const mapRef = useRef();
 
   const [userLocation, setUserLocation] = useState(null);
   const [events, setEvents] = useState({})
@@ -39,6 +42,14 @@ function MapScreen() {
 
   }, []);
 
+  const markers = [
+    {
+      name: "Paris",
+      latitude: 48.8666,
+      longitude: 2.3333
+    }
+  ]
+
 
   const focusMap = () => {
     const GreenBayStadium = {
@@ -54,19 +65,63 @@ function MapScreen() {
     console.log(region);
   }
 
+  const onMarkerSelected = (marker) => {
+    Alert.alert(marker.name)
+  }
+
   return (
     <View style={{ flex: 1 }}>
+      <Tabs.Screen options={{
+        headerLeft: () => (
+          <ScreenHeaderBtn
+            iconUrl={icons.left}
+            dimension='60%'
+            handlePress={() => router.back()}
+          />
+        ),
+        headerRight: () => (
+          <ScreenHeaderBtn iconUrl={icons.signOut} dimension='60%' handlePress={signOut} />
+        )
+      }}></Tabs.Screen>
       <Text> Map Screen</Text>
-      {/* <MapView style={StyleSheet.absoluteFill}
+      <MapView style={StyleSheet.absoluteFill}
         provider={PROVIDER_GOOGLE}
         initialRegion={INITIAL_REGION}
         showsUserLocation
         showsMyLocationButton
         onRegionChangeComplete={onRegionChange}
         ref={mapRef}
-      ></MapView> */}
+      >
+
+
+        {markers.map((marker, index) => (
+          <Marker key={index} coordinate={marker} onPress={() => onMarkerSelected(marker)}>
+          </Marker>
+        ))}
+
+        <View style={styles.tile}>
+          <Text>Paris</Text>
+          {/* Add more information here as needed */}
+        </View>
+
+      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  tile: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 100,
+    margin: 10,
+    borderRadius: SIZES.medium,
+    backgroundColor: 'white',
+    padding: 10,
+    alignItems: 'center',
+  },
+});
 
 export default MapScreen;
